15b2b81a28942edf6dc6edb6afe40b5b775bbbde
Pravin kumar <pravinkumarnattu@gmail.com>
2026-07-12 09:00:26 +0530

    some changes in main

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-for(let i=0; i<4; i++) {
-    console.log("this is bug branch");
-}
+
 for(let i=0; i<4; i++) {
     console.log("this is main branch");
+}
+
+for(let i=0; i<4; i++) {
+    console.log("this is bug branch");
 }
